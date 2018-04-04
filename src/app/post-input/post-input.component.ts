847d01e9b54357268dcd2145291748539bc9adc8
mariajesca <jescanobre@gmail.com>
2018-04-04 11:00:30 -0300

    ...

--- src/app/post-input/post-input.component.ts
+++ src/app/post-input/post-input.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../services/post.service';
-
+import { NgModel } from '@angular/forms';
+import { Post } from './../post-modelo/post-model';
 
 @Component({
   selector: 'app-post-input',
@@ -8,8 +9,18 @@ import { PostService } from '../services/post.service';
   styleUrls: ['./post-input.component.css']
 })
 export class PostInputComponent implements OnInit {
+  
+  id: Number;
+  nomeInput: String= "";
+  textoInput: String= "";   
+
   constructor (private postService:PostService) {}
+ 
   ngOnInit() {
   }
 
+  onSubmit(event){
+    event.preventDefault();
+    this.postService.addPost (new Post(this.id, this.nomeInput, this.textoInput, 0));
+  }
 }
